f295eb74979e7e9e81da4fa8b091b534b5b9a0ac
Roman <roman@slatestudio.com>
2013-08-07 08:29:18 +0300

    Set default cache expires in to 5 minutes

--- lib/crawlable_js/engine.rb
+++ lib/crawlable_js/engine.rb
@@ -5,5 +5,6 @@ module CrawlableJS
     config.crawlable_js = ActiveSupport::OrderedOptions.new
     config.crawlable_js.phantomjs_path   = File.join gem_root, 'bin/phantomjs'
     config.crawlable_js.phantomjs_script = File.join gem_root, 'bin/phantomjs_script.js'
+    config.crawlable_js.cache_expires_in = 5.minutes
   end
 end
